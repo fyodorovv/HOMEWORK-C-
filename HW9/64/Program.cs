a7004b288e087a3d9526edeeedd9d33bb0d960fa
Fedorov Vyacheslav <v.fyodorov@list.ru>
2022-11-30 02:53:33 +0300

    Выполненное домашнее задание 9-го урока

---
+++ HW9/64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2,"
+
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+EvenNumbers(number);
+
+void EvenNumbers(int number)
+{
+    if (number % 2 != 0) number = number-1;
+    if (number == 0) return;
+Console.Write($"{number}, ");
+EvenNumbers(number - 2);  
+}
---
+++ HW9/66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int number2 = int.Parse(Console.ReadLine());
+int sum = 0;
+SumNumbers(number1,number2,sum);
+void SumNumbers(int number1, int number2, int sum)
+{
+    if (number1 > number2) 
+    {
+        Console.WriteLine($"{sum}"); 
+        return;
+    }
+    sum = sum + (number1++);
+    SumNumbers(number1, number2, sum);
+}
