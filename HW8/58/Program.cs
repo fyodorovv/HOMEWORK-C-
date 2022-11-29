d7e5bc11ec9f1cf96c3100766e851ea62810935a
Fedorov Vyacheslav <v.fyodorov@list.ru>
2022-11-29 17:13:18 +0300

    Выполненное домашнее задание 8-го урока

---
+++ HW8/58/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 58: Задайте две матрицы. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// (посмотрите как реализуется произведение матриц, 
+// там не просто перемножение элемент на элемент)
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Write("Введите кол-во строк:");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите кол-во столбцов:");
+int columns = int.Parse(Console.ReadLine());
+int [,] firstArray = new int [rows,columns];
+int [,] secondArray = new int [rows,columns];
+int[,] resultArray = new int[rows, columns];
+GetArray(firstArray);
+PrintArray(firstArray);
+Console.WriteLine();
+GetArray(secondArray);
+PrintArray(secondArray);
+
+if (firstArray.GetLength(0) != secondArray.GetLength(1))
+{
+    Console.WriteLine(" Error ");
+    return;
+}
+for (int i = 0; i < firstArray.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < firstArray.GetLength(1); k++)
+        {
+            resultArray[i, j] += firstArray[i, k] * secondArray[k, j];
+        }
+    }
+}
+Console.WriteLine();
+PrintArray(resultArray);
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i,j] = new Random().Next(1,10);
+        }   
+    }
+
+}
+void PrintArray ( int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
---
+++ HW8/62/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 62. Напишите программу, которая заполнит 
+// спирально массив 4 на 4. 
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int [,] array = new int [4,4];
+int a = 4;
+GetArray(array,a);
+PrintArray(array);
+
+void GetArray(int[,] array, int a)
+{
+    int i = 0, j = 0;
+    int k = 0;
+    int value = 1;
+    for (int e = 0; e < a * a; e++)
+    {
+        do 
+        { 
+            array[i, j++] = value++; 
+        } 
+        while (++k < a - 1);
+        for (k = 0; k < a - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < a - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < a - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        a = a < 2 ? 0 : a - 2;
+    }
+}
+void PrintArray ( int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
